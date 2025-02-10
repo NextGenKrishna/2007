952fac749e86731966c172f1f7d1a4a757f14ec5
krishnagit2007dot <Ksb70160@gmail.com>
2025-02-10 12:35:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert("Welcome to DemandingHack!");
+function searchFunction() {
+    let query = document.getElementById("search-box").value.toLowerCase();
+    let result = document.getElementById("result");
+
+    if (query === "about") {
+        result.innerHTML = "You searched for 'About'. Click <a href='#'>here</a> to learn more.";
+    } else if (query === "contact") {
+        result.innerHTML = "You searched for 'Contact'. Click <a href='#'>here</a> to contact us.";
+    } else {
+        result.innerHTML = "No results found.";
+    }
+}
